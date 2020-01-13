9db2997b2d0ca3fc1ad7da6c24044fdeba1a4604
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-01-13 15:30:09 +0100

    - updates add views

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Login Page
+router.get('/login', (req, res) => res.send('Login'));
+
+// Register Page
+router.get('/register', (req, res) => res.send('Register'));
+
+module.exports = router;
