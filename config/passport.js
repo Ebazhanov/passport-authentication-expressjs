f682e2cbbace51add94021ede23ca8cf333150b1
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-01-17 11:20:40 +0100

    - updates

--- config/passport.js
+++ config/passport.js
@@ -4,37 +4,37 @@ const bcrypt = require('bcryptjs');
 // Load User model
 const User = require('../models/User');
 
-module.exports = function(passport) {
-  passport.use(
-    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-      // Match user
-      User.findOne({
-        email: email
-      }).then(user => {
-        if (!user) {
-          return done(null, false, { message: 'That email is not registered' });
-        }
+module.exports = function (passport) {
+    passport.use(
+        new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
+            // Match user
+            User.findOne({
+                email: email
+            }).then(user => {
+                if (!user) {
+                    return done(null, false, {message: 'That email is not registered'});
+                }
 
-        // Match password
-        bcrypt.compare(password, user.password, (err, isMatch) => {
-          if (err) throw err;
-          if (isMatch) {
-            return done(null, user);
-          } else {
-            return done(null, false, { message: 'Password incorrect' });
-          }
-        });
-      });
-    })
-  );
+                // Match password
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if (err) throw err;
+                    if (isMatch) {
+                        return done(null, user);
+                    } else {
+                        return done(null, false, {message: 'Password incorrect'});
+                    }
+                });
+            });
+        })
+    );
 
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
-
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
     });
-  });
+
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
+        });
+    });
 };
