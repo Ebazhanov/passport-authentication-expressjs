355a6e0789fa24caf2514bb907f1492945f44602
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-01-17 10:44:47 +0100

    - updates

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,70 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../models/Users');
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));
 
 // Register Page
 router.get('/register', (req, res) => res.render('register'));
+
+// Register Handler
+router.post('/register', (req, res) => {
+    const {name, email, password, password2} = req.body;
+    let errors = [];
+
+    // Check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({msg: 'Please fill in all fields'})
+    }
+
+    // Check passwords match
+    if (password !== password2) {
+        errors.push({msg: 'Passwords do not match'})
+    }
+
+    // Check pass length
+    if (password.length < 6) {
+        errors.push({msg: 'Password should be at least 6 characters'})
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        // Validation passed
+        User.findOne({email: email})
+            .then(user => {
+                if (user) {
+                    // User exists
+                    errors.push({msg: 'Email is already registered'});
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                    console.log(newUser);
+                    res.send('hello')
+                }
+            });
+    }
+});
 
 module.exports = router;
